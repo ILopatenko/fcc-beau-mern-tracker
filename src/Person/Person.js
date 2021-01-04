34ccc6989f485ff63fb84e4bcba91994a432e07c
Iurii Lopatenko <yalopatenko@gmail.com>
2021-01-04 15:50:43 -0800

    Project was dropped to a component-based style from course source files

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
-const person = (props) => {
+
+const person = ( props ) => {
     return (
         <div>
-            <p>My name is {props.name} and I'm {props.age} years old!</p>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
         </div>
     )
 };
-export default person
+
+export default person;
